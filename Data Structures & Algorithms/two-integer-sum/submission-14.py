ce3d998dd85424408f2def46eae9120dc7acc6a3
Sahil Sawant <sahilsawant15052006@gmail.com>
2026-08-14 18:16:08 +0530

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        map = {}
+        for i in range(0,len(nums)):
+            map[nums[i]]=i
+    
+        for i in range(0,len(nums)):
+            if target-nums[i] in map and i != map.get(target-nums[i]):
+                return [i,map.get(target-nums[i])]
+            
+        
